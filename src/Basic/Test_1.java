9d673d95c6ca4d28548202d3abbb6bc02b115453
pratiksha <pratiksha@LAPTOP-MBEQSGJA>
2022-10-03 15:52:44 +0530

    first commit

---
+++ src/Basic/Test_1.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Test_1 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("main method is running");
+	}
+	public static void m1()
+	{
+		System.out.println("hi");
+	}
+
+}
